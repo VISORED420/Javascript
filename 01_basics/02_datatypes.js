eef53cb67d3cae503ecb70873a7ea6ab1ae993f1
VISORED420 <vaibhavmathpal@gmail.com>
2025-02-18 15:40:35 +0530

    new

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+
+
+
+
+//alert("Hello World"); //using node js not browser
+
+console.log(3+3);
+//readable code
+console.log("Hitesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2^53 - 1
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique value
+
+
+//object
+
+console.log (typeof null);  //object
+console.log(typeof undefined); //undefined
+
